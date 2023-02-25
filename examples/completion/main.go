d9a2ca987b8847ae55b57934119e53efac1e01b3
JBD <rakyll@github.com>
2023-02-25 13:47:37 -0800

    Add completion example

---
+++ examples/completion/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/rakyll/openai-go"
+	"github.com/rakyll/openai-go/completion"
+)
+
+func main() {
+	ctx := context.Background()
+	s := openai.NewSession(os.Getenv("OPENAI_API_KEY"))
+
+	client := completion.NewClient(s, "text-davinci-003")
+	resp, err := client.Complete(ctx, &completion.Parameters{
+		N:         1,
+		MaxTokens: 7,
+		Prompt:    []string{"say this is a test"},
+	})
+	if err != nil {
+		log.Fatalf("Failed to complete: %v", err)
+	}
+
+	for _, choice := range resp.Choices {
+		log.Println(choice.Text)
+	}
+}
