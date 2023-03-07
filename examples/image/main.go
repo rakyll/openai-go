a037372cee625825a8724d0e2589724dd100e20b
ruokeqx <ruokeqx@163.com>
2023-03-08 00:54:45 +0800

    Add Image.Reader (#5)

--- examples/image/main.go
+++ examples/image/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"io/ioutil"
 	"log"
 	"os"
 
@@ -18,12 +19,21 @@ func main() {
 		N:      3,
 		Prompt: "a cute baby",
 		Size:   "1024x1024",
+		Format: "b64_json",
 	})
 	if err != nil {
 		log.Fatalf("Failed to generate image: %v", err)
 	}
 
 	for _, image := range resp.Data {
-		log.Println(image.URL)
+		reader, err := image.Reader()
+		if err != nil {
+			log.Fatalf("Failed to read image data: %v", err)
+		}
+		data, err := ioutil.ReadAll(reader)
+		if err != nil {
+			log.Fatalf("ReadAll error: %v", err)
+		}
+		log.Println(string(data[1:4]))
 	}
 }
