b455d9e7c39622fb7ba0354c76168c6cc359dbcd
Murilo Santana <mvrilo@gmail.com>
2023-03-27 21:19:10 -0300

    Add Name parameter for Message in CreateCompletionParams

--- chat/chat.go
+++ chat/chat.go
@@ -69,6 +69,7 @@ type Choice struct {
 type Message struct {
 	Role    string `json:"role,omitempty"`
 	Content string `json:"content,omitempty"`
+	Name    string `json:"name,omitempty"`
 }
 
 func (c *Client) CreateCompletion(ctx context.Context, p *CreateCompletionParams) (*CreateCompletionResponse, error) {
